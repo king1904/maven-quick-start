a9bde42caaaad627731562fd8a6ed140ce975e32
Youcef <youcef1904@gmail.com>
2020-04-01 16:08:28 +0200

    uso de StringUtils y añadir la dependencias lang3

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,14 +1,27 @@
 package clinic.programming.training;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Application {
     
     public Application() {
         System.out.println ("Inside Application");
+		
+		 
     }
+    
+    public int contarLetras(String palabras){
+		String[] separaPalabras=StringUtils.split(palabras," ");
+	
+	return (separaPalabras==null)?0 : separaPalabras.length;
+	}
 
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
 	Application app = new Application();
-    }
+	
+	int contador=app.contarLetras("Tengo cuatro palabras si.");
+    System.out.println("Palabras : "+contador);
+	}
 }
